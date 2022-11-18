7482b13dff82580050f9c7e58af08b5b674ea27a
patrick <roosterruf@gmail.com>
2022-11-17 18:41:49 -0700

    almost complete, minus highscore screen

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,20 @@ var startButton = document.getElementById('start-button');
 var nextButton = document.getElementById('next-button');
 var highscoreButton = document.getElementById('highscore-button');
 var goBackButton = document.getElementById('go-back-button');
+var submitButton = document.querySelector('#submit-button')
+
+var userInitials = document.querySelector('#user-initials')
+var userScore = document.querySelector('#user-score')
 
 var highscoreElement = document.getElementById('highscore-screen');
+var highscoreList = document.getElementById('highscore-list');
+
 var quizIntro = document.getElementById('quiz-intro');
 var questionContainerElement = document.getElementById('question-container');
 var questionElement = document.getElementById('question');
 var answerButtonsElement = document.getElementById('answer-buttons');
 var quizResultElement = document.getElementById('quiz-result-screen');
 
-//not working?
 var correctMessage = document.getElementById('correct-message')
 var wrongMessage = document.getElementById('wrong-message')
 
@@ -24,6 +29,8 @@ var quizComplete = false;
 
 var scoreElement = document.getElementById('score');
 
+var highScores = []
+
 startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
     currentQuestionIndex++
@@ -35,11 +42,29 @@ goBackButton.addEventListener('click', goBack);
 function getHighscores() {
     quizIntro.classList.add('hide');
     highscoreElement.classList.remove('hide');
+    //see renderLastRegistered on act22 - render highscores from local storage here?
+    var initials = localStorage.getItem('initials');
+    var score = localStorage.getItem('score');
+    var scores = localStorage.getItem('scores')
+    var liItem = document.createElement(
+        <li><span id="user-initials">PR</span> - <span id="user-score">XX</span></li>
+    )
+
+    for (let i = 0; i < 5; i++) {
+        liItem.innerText = `${scores[i].initials} - ${scores[i].score}`;
+    }
+
+    userInitials.textContent = initials;
+    userScore.textContent = score;
 }
 
 function goBack () {
     highscoreElement.classList.add('hide');
     quizIntro.classList.remove('hide');
+    timer = setInterval(function() {
+        timerElement.textContent = timerCount;
+    },);
+    timerCount = 100;
 }
 
 function startGame() {
@@ -52,25 +77,6 @@ function startGame() {
     setNextQuestion();
     startTimer();
 }
-
-// function startTimer() {
-//     function countdown() {
-//         var seconds = 60;
-//         function tick() {
-//             var counter = document.getElementById('counter');
-//             seconds--;
-//             counter.innerHTML =
-//                 "0:" + (seconds < 10 ? "0" : "") + String(seconds);
-//             if (seconds > 0) {
-//                 setTimeout(tick, 1000);
-//             }   else {
-                
-//             }
-//         }
-//         tick();
-//     }
-//     countdown();
-// }
 
 function startTimer() {
     timer = setInterval(function() {
@@ -91,31 +97,26 @@ function setNextQuestion() {
         quizResultElement.classList.remove('hide');
         scoreElement.textContent = timerCount;
     }
-    // if (currentQuestionIndex < questions.length) {
-    //     setNextQuestion
-    // }   else {
-    //     questionContainerElement.classList.add('hide');
-    //     quizResultElement.classList.remove('hide');
-    //     quizComplete = true;
-    // }
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question;
     question.answers.forEach(answer => {
         var button = document.createElement('button')
-        button.innerText = answer;
+        button.innerText = answer.text;
         button.classList.add('btn')
         if (answer.correct) {
             button.dataset.correct = answer.correct;
+        } else {
+            button.dataset.correct = false;
         }
+
         button.addEventListener('click', selectAnswer)
         answerButtonsElement.appendChild(button)
     });
 }
 
 function resetState() {
-    clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild
@@ -130,50 +131,64 @@ function selectAnswer(e) {
     console.log(correct)
     console.log(selectedButton)
 
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
+    setStatusClass(correct)
     nextButton.classList.remove('hide')
 }
 
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    // if (correct) {
-    //     element.classList.add('correct')
-    // } else {
-    //     element.classList.add('wrong')
-    // }
-    if (correct) {
-        correctMessage.classList.remove('hide')
+function setStatusClass(correct) {
+    if (correct === 'true') {
+        wrongMessage.classList.add('hide');
+        correctMessage.classList.remove('hide');
+        timerCount +=5
     }   else {
-        wrongMessage.classList.remove('hide')
+        wrongMessage.classList.remove('hide');
+        correctMessage.classList.add('hide');
+        timerCount -=5
     }
 }
 
-function clearStatusClass(element) {
-    // element.classList.remove('correct')
-    correctMessage.classList.add('hide')
-    // element.classList.remove('wrong')
-    wrongMessage.classList.add('hide')
-}
+// function clearStatusClass(element) {
+//     // element.classList.remove('correct')
+//     correctMessage.classList.add('hide')
+//     // element.classList.remove('wrong')
+//     wrongMessage.classList.add('hide')
+// }
+
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    var initials = document.querySelector('#initials').value;
+    var score = timerCount
+    // highScores.push(`${initials - score}`)
+    highScores.push({initials: initials, score: score});
+
+
+    // localStorage.setItem('initials', initials);
+    // localStorage.setItem('score', score)
+    localStorage.setItem('scores', JSON.stringify(highScores))
+
+    console.log(highScores)
+
+    getHighscores()
+    quizResultElement.classList.add('hide')
+})
+
 
 var questions = [
     {
         question: "Commonly used data types do NOT include:",
         answers: 
-        // [
-        //     { text:"strings", correct: false },
-        //     { text:"booleans", correct: false },
-        //     { text:"numbers", correct: false },
-        //     { text: "alerts", correct: true },
-        // ]
-        ['strings', 'booleans', 'numbers', 'alerts'],
-        correct: 'alerts',
+        [
+            { text:"strings", correct: false },
+            { text:"booleans", correct: false },
+            { text:"numbers", correct: false },
+            { text: "alerts", correct: true },
+        ]
     },
     {
         question: "What does CSS stand for?",
-        answers: [
+        answers:
+        [
             { text:"Central Style Sheets", correct: false },
             { text:"Cascading Style Sheets", correct: true },
             { text:"Cascading Simple Sheets", correct: false },
@@ -182,7 +197,8 @@ var questions = [
     },
     {
         question: "Which language runs in a web browser?",
-        answers: [
+        answers:
+        [
             { text:"javascript", correct: true },
             { text:"C", correct: false },
             { text:"Java", correct: false },
@@ -191,7 +207,8 @@ var questions = [
     },
     {
         question: "What does HTML stand for?",
-        answers: [
+        answers:
+        [
             { text:"Hyperloop Machine Language", correct: false },
             { text:"H T M L", correct: false },
             { text:"Hypertext Markup Language", correct: true },
@@ -200,7 +217,8 @@ var questions = [
     },
     {
         question: "What year was Paul McCartney born?",
-        answers: [
+        answers:
+        [
             { text:"1917", correct: false },
             { text:"2077", correct: false },
             { text:"1999", correct: false },
@@ -209,7 +227,8 @@ var questions = [
     },
     {
         question: "",
-        answers: [
+        answers:
+        [
             { text:"", correct: false },
             { text:"", correct: false },
             { text:"", correct:  false},
